d0d187d159639f1a035b518f5bf2ec302136ef5d
devabacus <32705417+devabacus@users.noreply.github.com>
2025-12-06 07:08:18 +0500

    create migrations

--- todo_flutter/lib/core/config/config.dart
+++ todo_flutter/lib/core/config/config.dart
@@ -10,28 +10,28 @@ class AppConfig {
 
   static String get baseUrl {
     // В режиме отладки выбираем локальный URL
-    // return dotenv.env['BASE_URL'] ?? '';
+    return dotenv.env['BASE_URL'] ?? '';
 
-    if (kDebugMode) {
-      // Сначала проверяем, является ли платформа вебом
-      if (kIsWeb) {
-        // Для веба всегда используем localhost
-        return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ?? 'http://localhost:8080/';
-      } else {
-        // Теперь можно безопасно использовать dart:io
-        if (Platform.isAndroid) {
-          // Для эмулятора Android используем специальный адрес
-          return dotenv.env['LOCAL_BASE_URL_ANDROID'] ??
-              'http://10.0.2.2:8080/';
-        } else {
-          // Для Windows и других настольных платформ используем localhost
-          return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ??
-              'http://localhost:8080/';
-        }
-      }
-    } else {
-      // Для релизной сборки используем удаленный сервер
-      return dotenv.env['BASE_URL'] ?? '';
-    }
+    // if (kDebugMode) {
+    //   // Сначала проверяем, является ли платформа вебом
+    //   if (kIsWeb) {
+    //     // Для веба всегда используем localhost
+    //     return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ?? 'http://localhost:8080/';
+    //   } else {
+    //     // Теперь можно безопасно использовать dart:io
+    //     if (Platform.isAndroid) {
+    //       // Для эмулятора Android используем специальный адрес
+    //       return dotenv.env['LOCAL_BASE_URL_ANDROID'] ??
+    //           'http://10.0.2.2:8080/';
+    //     } else {
+    //       // Для Windows и других настольных платформ используем localhost
+    //       return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ??
+    //           'http://localhost:8080/';
+    //     }
+    //   }
+    // } else {
+    //   // Для релизной сборки используем удаленный сервер
+    //   return dotenv.env['BASE_URL'] ?? '';
+    // }
   }
 }
